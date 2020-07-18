5a0923d5c19374d42836765754efd9b31a76c067
athlaliel <athla8452@gmail.com>
2020-07-18 14:29:40 +0900

    second
    コールバック変更反映される？

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :ninjas
   root 'ninjas#index'
+  post '/callback' => 'webhook#callback'
 end
